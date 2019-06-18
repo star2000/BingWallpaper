7323dc9363347cc7c55bd9dce8a5034a16ace932
星 <i@star2000.work>
2019-06-18 19:22:45 +0800

    修复：壁纸自动刷新

--- wallup.ps1
+++ wallup.ps1
@@ -1,13 +1,24 @@
 #download correct resolution image
-$Market = "en-US"
+$Market = "zh-CN"
 $Resolution = "1920x1080"
 $FileName = "wallpaper.jpg"
 $PictureFolder = "$env:USERPROFILE\Pictures"
-$PicturePath = "$($PictureFolder)\$($FileName)"
-[ xml ]$Bingxml = (New-Object System.Net.WebClient).DownloadString("http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=$($Market)");
-$ImageUrl = "http://www.bing.com$($Bingxml.images.image.urlBase)_$($Resolution).jpg";
-Invoke-WebRequest -UseBasicParsing -Uri $ImageUrl -OutFile "$PicturePath";
+$PicturePath = "$PictureFolder\$FileName"
+[xml]$Bingxml = (New-Object System.Net.WebClient -Property @{Encoding = [Text.Encoding]::UTF8 }).DownloadString("http://bing.com/HPImageArchive.aspx?n=1&mkt=$Market");
+$ImageUrl = "http://bing.com$($Bingxml.images.image.urlBase)_$Resolution.jpg";
+(New-Object System.Net.WebClient).DownloadFile($ImageUrl, $PicturePath)
 
 #set image as wallpaper
-set-itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaper -value "$PicturePath";
-rundll32.exe user32.dll, UpdatePerUserSystemParameters, 0, $false
+Set-ItemProperty -Path 'HKCU:Control Panel\Desktop' -Name 'WallPaper' -Value $PicturePath;
+# from https://www.joseespitia.com/2017/09/15/set-wallpaper-powershell-function
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Params
+{
+    [DllImport("User32.dll",CharSet=CharSet.Unicode)]
+    public static extern int SystemParametersInfo (Int32 uAction, Int32 uParam, String lpvParam, Int32 fuWinIni);
+}
+"@
+[Params]::SystemParametersInfo(20, 0, $PicturePath, 3)
