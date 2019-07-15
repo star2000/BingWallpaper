3d120532dd4ca7fb0ac4c9aba4d8b0345da5c916
星 <i@star2000.work>
2019-07-15 11:22:15 +0800

    简化

--- wallup.ps1
+++ wallup.ps1
@@ -1,24 +1,15 @@
-#download correct resolution image
-$Market = "zh-CN"
-$Resolution = "1920x1080"
-$FileName = "wallpaper.jpg"
-$PictureFolder = "$env:USERPROFILE\Pictures"
-$PicturePath = "$PictureFolder\$FileName"
-[xml]$Bingxml = (New-Object System.Net.WebClient -Property @{Encoding = [Text.Encoding]::UTF8 }).DownloadString("http://cn.bing.com/HPImageArchive.aspx?n=1&mkt=$Market");
-$ImageUrl = "http://cn.bing.com$($Bingxml.images.image.urlBase)_$Resolution.jpg";
-(New-Object System.Net.WebClient).DownloadFile($ImageUrl, $PicturePath)
+Param (
+    $Path = "$env:USERPROFILE\Pictures\wallpaper.jpg",
+    $Resolution = '1920x1080',
+    $ensearch = 0,
+    $idx = 0
+)
 
-#set image as wallpaper
-Set-ItemProperty -Path 'HKCU:Control Panel\Desktop' -Name 'WallPaper' -Value $PicturePath;
-# from https://www.joseespitia.com/2017/09/15/set-wallpaper-powershell-function
-Add-Type -TypeDefinition @"
-using System;
-using System.Runtime.InteropServices;
+#下载壁纸
+[xml]$Bing = (New-Object System.Net.WebClient -Property @{Encoding = [Text.Encoding]::UTF8 }).DownloadString("http://www.bing.com/HPImageArchive.aspx?n=1&idx=$idx&ensearch=$ensearch")
+$ImageUrl = "http://www.bing.com$($Bing.images.image.urlBase)_$Resolution.jpg"
+(New-Object System.Net.WebClient).DownloadFile($ImageUrl, $Path)
 
-public class Params
-{
-    [DllImport("User32.dll",CharSet=CharSet.Unicode)]
-    public static extern int SystemParametersInfo (Int32 uAction, Int32 uParam, String lpvParam, Int32 fuWinIni);
-}
-"@
-[Params]::SystemParametersInfo(20, 0, $PicturePath, 3)
+# 设置壁纸
+(Add-Type -MemberDefinition '[DllImport("user32.dll")]public static extern bool 
+SystemParametersInfo (uint uiAction, uint uiParam, String pvParam, uint fWinIni);' -Name 'Params' -PassThru)::SystemParametersInfo(20, 0, $Path, 3)
