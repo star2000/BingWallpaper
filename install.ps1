4f1941212c54f78ee9fcbad8bf9a08459ca1fae5
星 <i@star2000.work>
2019-10-24 10:33:05 +0800

    隐藏运行时弹出的窗口

--- install.ps1
+++ install.ps1
@@ -1,7 +1,11 @@
+New-Item $env:USERPROFILE\BingWallpaper.js -Value "(new ActiveXObject('WScript.Shell')).run('powershell -NoProfile -NonInteractive iwr -useb github.com/star2000/BingWallpaper/raw/master/wallup.ps1 | iex', 0)" -Force | Out-Null
+
 Register-ScheduledTask BingWallpaper star2000 (
-    New-ScheduledTaskAction powershell { -NoLogo -NoProfile -NonInteractive -WindowStyle Hidden iwr -useb github.com/star2000/BingWallpaper/raw/master/wallup.ps1 | iex }
+    New-ScheduledTaskAction $env:USERPROFILE\BingWallpaper.js
 ) (
     New-ScheduledTaskTrigger -Daily -At 0:0
 ) (
     New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -RunOnlyIfNetworkAvailable -StartWhenAvailable
-)
+) -Force | Out-Null
+
+Start-ScheduledTask BingWallpaper star2000
