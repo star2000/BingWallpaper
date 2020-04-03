c9e51a7026e6eaaccdc457152a7c9fdd4a87db50
星 <i@star2000.work>
2020-04-03 19:56:17 +0800

    卸载时恢复默认壁纸

--- uninstall.ps1
+++ uninstall.ps1
@@ -1,2 +1,17 @@
 schtasks /Delete /TN '\star2000\BingWallpaper' /F
 Remove-Item "$env:ALLUSERSPROFILE\BingWallpaper.vbs" -Force
+
+# set wallpaper to default
+$Wallpaper = "$env:SystemRoot\Web\Wallpaper\Windows\img0.jpg"
+$WallpaperBitmap = "$env:TMP\wallpaper.bmp"
+
+$ErrorActionPreference = 'Stop'
+
+Add-Type -Assembly System.Drawing
+$Bitmap = [Drawing.Bitmap]$Wallpaper
+$Bitmap.Save($WallpaperBitmap, 'bmp')
+$Bitmap.Dispose()
+
+(Add-Type '[DllImport("user32.dll")]public static extern bool SystemParametersInfo (uint uiAction, uint uiParam, String pvParam, uint fWinIni);' -Name '_' -PassThru)::SystemParametersInfo(20, 0, $WallpaperBitmap, 3)
+
+Remove-Item $WallpaperBitmap -Force
